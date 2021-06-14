c30549adf012310673553201a3713cdcdbfc92cd
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-14 11:23:45 +0200

    commit on master -m nested two children in appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Activities from "./components/Pages/Profile/Formation/FormationSection/Activities";
+import Skills from "./components/Pages/Profile/Formation/FormationSection/Skills";
 
 function App() {
   return (
     <div className="App">
-
+      <Activities />
+      <Skills />
     </div>
   );
 }
---
+++ frontend/src/components/Pages/Profile/Formation/FormationSection/Skills.jsx
@@ -0,0 +1,16 @@
+import { Card, Row, Col } from "react-bootstrap";
+const Skills = () => {
+  return (
+    <>
+      <Card>
+        <Row>
+          <Col>
+            <h1>Skills & endorsements</h1>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+};
+
+export default Skills;
