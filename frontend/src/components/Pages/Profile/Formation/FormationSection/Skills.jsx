975f407381ad95e422897285a5680911eca40459
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-14 12:51:00 +0200

    commit on master -m push before lunch

--- frontend/src/components/Pages/Profile/Formation/FormationSection/Skills.jsx
+++ frontend/src/components/Pages/Profile/Formation/FormationSection/Skills.jsx
@@ -1,14 +1,25 @@
-import { Card, Row, Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import styles from "../../../../../modules/skills.module.css";
+import skillList from "../../../../../data/skills.json";
+import { IconContext } from "react-icons";
+import { BsPencil } from "react-icons/bs";
+import { useState, useEffect } from "react";
 const Skills = () => {
+  const [skills, setSkills] = useState(skillList.skills);
   return (
     <>
-      <Card>
-        <Row>
-          <Col>
-            <h1>Skills & endorsements</h1>
-          </Col>
-        </Row>
-      </Card>
+      <div className={styles.container}>
+        <h2 className={styles.header}>Skills & endorsements</h2>
+        <div className={styles.innerContainer}>
+          <p className={styles.newSkill}>Add a new skill</p>
+          <IconContext.Provider value={{ className: styles.icon }}>
+            <div>
+              <BsPencil />
+            </div>
+          </IconContext.Provider>
+        </div>
+        <Button variant="outline-primary">Take skill quiz</Button>
+      </div>
     </>
   );
 };
