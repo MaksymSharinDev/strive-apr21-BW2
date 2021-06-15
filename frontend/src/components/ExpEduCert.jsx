caef1cbfe76891f2d5df5ac442714e7d9e9f9d32
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-15 17:07:54 +0200

    commit on master -m experience post not working

--- frontend/src/components/ExpEduCert.jsx
+++ frontend/src/components/ExpEduCert.jsx
@@ -1,9 +1,14 @@
 import { Card } from "react-bootstrap";
+import Experience from "./ExpEduCertComponents/Experience";
 const ExpEduCert = () => {
   return (
     <>
       <Card>
-        <h1> bunch of stuff</h1>
+        <Experience />
+        <hr />
+        <h2>Education</h2>
+        <hr />
+        <h2>Licences and certification</h2>
       </Card>
     </>
   );
