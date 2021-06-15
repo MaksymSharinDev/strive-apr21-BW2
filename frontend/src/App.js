ba166c80849fb794ed20314f52d4f20c186b16b7
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-15 11:41:33 +0200

    commit on master -m nested About in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,43 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Col, Container, Row} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 import Activities from "./components/Pages/Profile/Formation/FormationSection/Activities";
 import Skills from "./components/Pages/Profile/Formation/FormationSection/Skills";
-import Sidebar from './components/Pages/Profile/Sidebar/SidebarContainer/SidebarContainer.jsx'
-import MyFooter from './components/Pages/Profile/MyFooter';
-import MyNavbar from './components/Pages/Profile/MyNavbar';
-
+import Sidebar from "./components/Pages/Profile/Sidebar/SidebarContainer/SidebarContainer.jsx";
+import MyFooter from "./components/Pages/Profile/MyFooter";
+import MyNavbar from "./components/Pages/Profile/MyNavbar";
+import About from "./components/Pages/Profile/Formation/About";
 
 function App() {
-    return (
-        <div className="App">
-            <Container fluid>
-                <nav style={{backgroundColor: 'white', borderBottom: 'gray'}}>
-                    <Container>
-                        <MyNavbar/>
-                    </Container>
-                </nav>
-            </Container>
-            <Container className={'mt-5'}>
-                <Row>
-                    <Col xs={8}>
-                        <main>
-                            <Activities/>
-                            <Skills/>
-                        </main>
-                    </Col>
-                    <Col xs={4}>
-                        <aside>
-                            <Sidebar/>
-                        </aside>
-                    </Col>
-                </Row>
-            </Container>
-            <Container >
-                <MyFooter/>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Container fluid>
+        <nav style={{ backgroundColor: "white", borderBottom: "gray" }}>
+          <Container>
+            <MyNavbar />
+          </Container>
+        </nav>
+      </Container>
+      <Container className={"mt-5"}>
+        <Row>
+          <Col xs={8}>
+            <main>
+              <About />
+              <Activities />
+              <Skills />
+            </main>
+          </Col>
+          <Col xs={4}>
+            <aside>
+              <Sidebar />
+            </aside>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <MyFooter />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
