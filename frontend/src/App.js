926d8c39be9adac976aa532eb0a79ed753083f56
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-16 12:37:36 +0200

    feat: refactor iteration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,66 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
-import Activities from "./components/Pages/Profile/Formation/FormationSection/Activities";
-import Skills from "./components/Pages/Profile/Formation/FormationSection/Skills";
-import Sidebar from "./components/Pages/Profile/Sidebar/SidebarContainer/SidebarContainer.jsx";
-import MyFooter from "./components/Pages/Profile/MyFooter";
+import './App.css'
+
+import {Col, Container, Row} from "react-bootstrap";
+
 import MyNavbar from "./components/Pages/Profile/MyNavbar";
-import About from "./components/Pages/Profile/Formation/About";
-import ExpEduCert from "./components/ExpEduCert";
+import Profile from './components/Pages/Profile/Profile.jsx'
+import Feed from "./components/Pages/Feed/Feed";
+
+import Sidebar from "./components/Pages/Profile/Sidebar/SidebarContainer/SidebarContainer";
+import MyFooter from "./components/Pages/Profile/MyFooter";
+
+
 
 function App() {
-  return (
-    <div className="App" style={{backgroundColor: 'darkcyan'}}>
-      <style>
-        {'main .card{padding: 25px;}\n'}
-      </style>
-      <Container fluid className={'p-0'}>
-        <nav style={{ backgroundColor: "white", borderBottom: "gray" }}>
-          <Container>
-            <MyNavbar />
-          </Container>
-        </nav>
-      </Container>
-      <Container className={"mt-5"}>
-        <Row>
-          <Col xs={8}>
-            <main>
-              <About />
-              <Activities />
-              <ExpEduCert />
-              <Skills />
-            </main>
-          </Col>
-          <Col xs={4}>
-            <aside>
-              <Sidebar />
-            </aside>
-          </Col>
-        </Row>
-      </Container>
+    return (
+        <div className="App" style={{backgroundColor: 'darkcyan'}}>
 
-        <Container fluid style={{backgroundColor: 'white', padding: '5px',marginTop: '10px'}}>
-            <MyFooter />
-
-        </Container>
-    </div>
-  );
+            <Router>
+                <Container fluid className={'p-0'}>
+                    <nav style={{backgroundColor: "white", borderBottom: "gray"}}>
+                        <Container>
+                            <MyNavbar/>
+                        </Container>
+                    </nav>
+                </Container>
+                <Container className={"mt-5"}>
+                    <Row>
+                        <Col xs={8}>
+                            <main>
+                                <Switch>
+                                    <Route path="/">
+                                        <Profile/>
+                                    </Route>
+                                    <Route path="/profile">
+                                        <Profile/>
+                                    </Route>
+                                    <Route path="/feed">
+                                        <Feed/>
+                                    </Route>
+                                </Switch>
+                            </main>
+                        </Col>
+                        <Col xs={4}>
+                            <aside>
+                                <Sidebar/>
+                            </aside>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container fluid style={{backgroundColor: 'white', padding: '5px', marginTop: '10px'}}>
+                    <MyFooter/>
+                </Container>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
