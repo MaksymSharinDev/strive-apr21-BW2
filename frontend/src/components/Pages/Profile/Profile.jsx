7ffd036597110dfda2e197a1e952e5428bacb552
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-16 12:37:45 +0200

    feat: refactor iteration

---
+++ frontend/src/components/Pages/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+
+import Activities from "./Formation/FormationSection/Activities";
+import Skills from "./Formation/FormationSection/Skills";
+import About from "./Formation/About";
+import ExpEduCert from "../../ExpEduCert";
+
+
+const Profile = () => (
+    <>
+        <About/>
+        <Activities/>
+        <ExpEduCert/>
+        <Skills/>
+    </>
+)
+export default Profile
